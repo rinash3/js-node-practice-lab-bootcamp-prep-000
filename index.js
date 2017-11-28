6d3a700754457b77b5be358673cff90af75c83ea
Rina razumov <rinash3@gmail.com>
2017-11-28 20:18:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "rina"
-var height = "161"
+var height = 161
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
